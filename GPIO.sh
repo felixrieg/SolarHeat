7df000663672d8e64faac74472f7f36896887e18
Felix <rieg_felix@web.de>
2024-06-03 17:14:06 +0200

    feat: improve GPIO script

--- GPIO.sh
+++ GPIO.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+lastOutput=""
+oldPin="25"
+oldStatus="false"
+
+# Check if raspi-gpio is installed
+if ! command -v raspi-gpio &> /dev/null
+then
+    echo "raspi-gpio not found, please install it"
+    exit 1
+fi
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq not found, please install it"
+    exit 1
+fi
+
 while :; 
 do 
     response=$(curl -s "http://localhost:8080/status")
@@ -9,25 +27,33 @@ do
     # Check if we got a response
     if [ -z "$response" ]
     then
-        echo "No response"
+        newOutput="No response"
     else
-        echo "Recieved response - Status: $status, Pin: $pin"
-        
-        # Check if raspi-gpio is installed
-        if command -v raspi-gpio &> /dev/null
+        newOutput="Received response: $response"
+        if [ "$oldPin" != "$pin" ] || [ "$oldStatus" != "$status" ]
         then
-            echo $(raspi-gpio get $pin)
-            echo $(raspi-gpio set $pin op)
-            if $status == "true"
+            # Something changed
+            if [ "$oldPin" != "$pin" ]
             then
-                echo "Pin high"
-                echo $(raspi-gpio set $pin dh)
-            else
-                echo "Pin low"
-                echo $(raspi-gpio set $pin dl)
+                newOutput="$newOutput\n\tset old pin($oldPin) low: $(raspi-gpio set $pin op dl)"
             fi
+
+            if [ "$status" == "true" ]
+            then
+                newOutput="$newOutput\n\tset pin($pin) high: $(raspi-gpio set $pin op dh)"
+            else
+                newOutput="$newOutput\n\tset pin($pin) low: $(raspi-gpio set $pin op dl)"
+            fi
+
+            oldPin=$pin
+            oldStatus=$status
         fi
     fi
 
-    sleep 2; 
+    if [ "$lastOutput" != "$newOutput" ] && [ "$newOutput" != "" ]
+    then
+        echo -e "$(date '+%Y-%m-%d %H:%M:%S') -  $newOutput"
+        lastOutput=$newOutput
+    fi
+    sleep 3; 
 done
